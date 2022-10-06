ccd34763dfa3a87b4e571c47ec09eb4eb2df3443
erayBatigun <eraybtgn24@gmail.com>
2022-10-06 20:03:59 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atmApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bakiye = 1000;
+            int yatirilanPara;
+            int cekilenPara;
+            Console.WriteLine("Hoşgeldiniz lütfen yapmak istediğiniz işlemi seçiniz");
+            Console.WriteLine("1: Bakiye sorgulama");
+            Console.WriteLine("2: Para yatır");
+            Console.WriteLine("3: Para çek");
+            Console.WriteLine("4: Çıkış");
+            tekrar:
+            string tus = Console.ReadLine();
+
+            if(tus == "1")
+            {
+                Console.WriteLine("Bakiyeniz "+ bakiye);
+                Console.ReadLine();
+            }else if (tus == "2")
+            {
+                yatirilanPara = Convert.ToInt32(Console.ReadLine());
+                bakiye += yatirilanPara;
+                Console.WriteLine("Yeni bakiye: " + bakiye);
+                Console.ReadLine();
+                
+            }else if (tus == "3")
+            {
+                cekilenPara = Convert.ToInt32(Console.ReadLine());
+                bakiye -= cekilenPara;
+                Console.WriteLine("Yeni bakiye: "+ bakiye);
+                Console.ReadLine();
+
+            }else if(tus == "4")
+            {
+                Console.WriteLine("Çıkış yapılıyor...");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Hatalı tuslama ");
+                goto tekrar;
+            }
+        }
+    }
+}
---
+++ if/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int not1 = 90;
+            int not2 = 90;
+            int ortalama = (not1 + not2) / 2;
+            if (ortalama >= 90)
+            {
+                Console.WriteLine("Notunuz A ");
+                Console.ReadLine();
+
+            }
+            else if (ortalama >= 80)
+            {
+                Console.WriteLine("Notunuz B ");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("kaldınız");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
